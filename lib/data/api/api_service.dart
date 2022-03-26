adba6a34bafb505d24afc0e7b0198465b117fb27
muadzfathulloh <muadzfathulloh@gmail.com>
2022-03-26 17:07:09 +0700

    Percobaan Article

---
+++ lib/data/api/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../model/article.dart';
+
+class ApiService {
+  static final String _baseUrl = 'https://newsapi.org/v2/';
+  static final String _apiKey = '177fad0842d64e09a5261932c2d3f60b';
+  static final String _category = 'business';
+  static final String _country = 'id';
+
+  Future<ArticlesResult> topHeadlines() async {
+    final response = await http.get(Uri.parse(_baseUrl +
+        "top-headlines?country=$_country&category=$_category&apiKey=$_apiKey"));
+    if (response.statusCode == 200) {
+      return ArticlesResult.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load top headlines');
+    }
+  }
+}
